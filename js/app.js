659dda27b041dcbc0215f7648fed97de98bf1ef7
Anthony Simmmons <simmonsar@gmail.com>
2017-03-25 23:40:03 -0400

    variable and function  definition

--- js/app.js
+++ js/app.js
@@ -1,6 +1,53 @@
 $(function(){
 
 console.log('card game files attached');
-
-
 }); // onload ends
+
+
+// ================
+// Variables
+// ================
+
+// player
+// -----------
+var player1CardsPlayed = [];  //array of p1 cards played in the game
+var player2CardsPlayed = [];  //array of p2 cards played in the game
+var player1Points = 0;
+var player2Points = 0;
+var player1rdsWon = 0;
+var player2rdsWon = 0;
+var deckCardsremaning  = 0;
+var rdsPlayed = 0;
+
+
+
+// functions
+// -------------
+var gameWinner = function () {  // function to determine the gameWinner
+
+}
+
+
+var startRd   = function() {  // checks to see if rds played < 2
+
+}
+
+
+var roundWinner = function () { //checks if p1 pts > p2 pts
+                                //checks if cards have turned 6 times
+                                //increments rd counter
+}
+
+
+var endOfRound = function () {    //check if each player has touched 3 cards
+
+}
+
+var cardTouch = function(){}  //clicks card and turns it over
+
+var rdsWonTotal =  function () { // var player1rdsWon - var player2rdsWon
+
+}
+
+var player1Points = function() { player1Points +=}
+var player2Points = function() { player2Points +=}
