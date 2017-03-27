e444a58537bf2fec8b678aeb6e6ac1ecb2308f29
Anthony Simmmons <simmonsar@gmail.com>
2017-03-26 22:56:59 -0400

    card turn onclick working

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
 $(function(){
+var $card = $('.card');
+  var $Cardturn   = function() {             // checks to see if rds played <
+    console.log("I've been clicked");
+  };
+    $card.on('click',$Cardturn);
+
+
 
 console.log('card game files attached');
 }); // onload ends
 
 
-                              // ================
-                              // Variables
-                              // ================
+// ================
+// Variables
+// ================
 
-                              // player
-                              // -----------
+var deckCards =                       //array of all cards to be played
+[{name: "Bulbasaur", damage:60},{name: "Caterpie", damage:40},
+{name: "Charmander", damage:60},{name: "Clefairy", damage:50},
+{name: "Jigglypuff", damage:60},{name: "Mankey", damage:30},
+{name: "Meowth", damage:60},{name: "Nidoran - female", damage:60},
+{name: "Nidoran - male", damage:50},{name: "Oddish", damage:40},
+{name: "Pidgey", damage:50},{name: "Pikachu", damage:50},
+{name: "Poliwag", damage:50},{name: "Psyduck", damage:60},
+{name: "Rattata", damage:30}, {name: "Squirtle", damage:60},
+{name: "Vulpix", damage:50}, {name: "Weedle", damage:40}];
+
+var player1Cards = [];        //array of p1 cards
+var player2Cards = [];        //array of p2 cards
 var player1CardsPlayed = [];  //array of p1 cards played in the game
 var player2CardsPlayed = [];  //array of p2 cards played in the game
 var carPoints = [];           //array of all cards for play
@@ -17,13 +35,14 @@ var player1Points = 0;
 var player2Points = 0;
 var player1rdsWon = 0;
 var player2rdsWon = 0;
-var deckCardsremaning  = 0;
+var deckCardsremaning  = 18;
 var rdsPlayed = 0;
+// var $card = $('.card');
 
 
 
-                              // functions
-                              // -------------
+// functions
+// -------------
 //---------------------
 var gameWinner = function () {}   // function to determine the gameWinner
 //---------------------
@@ -33,17 +52,45 @@ var gameWinner = function () {}   // function to determine the gameWinner
 
 //---------------------
 var gameStart = function() {
-  alert('Would you like to play a game of BigBank?', 'Y,N');
-  startRd();
+  var input = prompt('Would you like to play a game of BigBank?', 'Y,N');
+    if (input = 'Y') {
+      dealCards();
+    }
+
+};
+//---------------------
+// Deal cards
+// places cards into player card arrays
+// I am trying to get 3 cards assigned from the deckCards array to player1Cards
+// then give player1Cards and player2Cards 3 cards and decrement deckCards by 6
+// assign items from deckCards to the three cards given to players
+//---------------------
+var dealCards = function(){
+
+  for (var i = 1; i < deckCards.length; i++) {
+    if (deckCards.length > 6) {
+      player1Cards = deckCards.name;
+      player2Cards = deckCards.name;
+      deckCardsremaning-=6;
+
+      console.log("player1 cards" + player1Cards);
+      console.log("player2 cards" + player2Cards);
+      console.log('deckCardsremaning' + deckCardsremaning);
+    }
+    else {
+      console.log('not enough cards');
+  }
+  }
 };
 //---------------------
 // startRd
 // is a function to turn over the cards on click to reveal the card number
+// create a listner for the cards
 //---------------------
-var startRd   = function() {      // checks to see if rds played < 2
-
-  console.log('Its the start of the round');
-};
+// var $Cardturn   = function() {             // checks to see if rds played <
+//   console.log("I've been clicked");
+// };
+//   $card.on('click',$Cardturn);
 
 //---------------------
 var roundWinner = function ()  {} // checks if p1 pts > p2 pts
